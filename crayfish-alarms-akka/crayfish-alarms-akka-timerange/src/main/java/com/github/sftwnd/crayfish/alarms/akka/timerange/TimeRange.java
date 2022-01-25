a9b2b6d20469be125fc44e9a7965a0bad26264e4
Andrey Shindarev <ashindarev@gmail.com>
2022-01-25 21:30:55 +0300

    TimeRange AKKA Actor has been realized. FYI: Need to hide TimeRangeItems from Actor. It had to be created inside the Actor.

---
+++ crayfish-alarms-akka/crayfish-alarms-akka-timerange/src/main/java/com/github/sftwnd/crayfish/alarms/akka/timerange/TimeRange.java
@@ -0,0 +1,241 @@
+/*
+ * Copyright © 2017-20xx Andrey D. Shindarev. All rights reserved.
+ * This program is made available under the terms of the BSD 3-Clause License.
+ * Contacts: ashindarev@gmail.com
+ */
+package com.github.sftwnd.crayfish.alarms.akka.timerange;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.TimerScheduler;
+import com.github.sftwnd.crayfish.alarms.timerange.TimeRangeItems;
+import lombok.NonNull;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import static java.util.Optional.ofNullable;
+
+/**
+ * Интерфейс протокола Typed Akka Actor для TimeRange по регистрации будильников и их обработке
+ */
+public interface TimeRange {
+
+    /**
+     * Базовый интерфейс для описания типа входящих сообщений
+     * @param <X> generic для описания содержимого команды
+     */
+    interface Command<X> {}
+
+    /**
+     * Процессор, содержащий в себе TimeRangeItems структуру с будильниками. Занимается сохранением входящих будильников и
+     * инициализацией реакции на их срабатывание.
+     * @param <M> Тип входящего сообщения на регистрацию будильника
+     * @param <R> Тип уходящего в обработку объекта по факту срабатывания будильника
+     */
+    class TimeRangeProcessor<M,R> {
+
+        // Это кратчайшее описание приведения, по этой причине выбрано именно оно
+        @SuppressWarnings({"uncheched", "rawtype"})
+        private final Class<Command<M>> COMMAND = (Class<Command<M>>)((Class<? extends Command>) Command.class);
+        @SuppressWarnings({"uncheched", "rawtype"})
+        private final Class<AddCommand<M>> ADD_COMMAND = (Class<AddCommand<M>>)((Class<? extends AddCommand>) AddCommand.class);
+        private final Command<M> TIMEOUT = new Command<>() {};
+
+        private final TimerScheduler<Command<M>> timers;
+        private final TimeRangeItems<M,R> timeRange;
+        private final Consumer<Collection<R>> firedConsumer;
+        private final Duration checkDelay;
+
+        private TimeRangeProcessor(
+                @NonNull  TimerScheduler<Command<M>> timers,
+                @NonNull  TimeRangeItems<M,R> timeRangeItems,
+                @NonNull  Consumer<Collection<R>> firedConsumer,
+                // В пределах TimeRangeItems::interval можно задать значение насколько раньше считать запись сработавшей...
+                @Nullable Duration withCheckDelay
+        ) {
+            this.timers = timers;
+            this.timeRange = timeRangeItems;
+            this.firedConsumer = firedConsumer;
+            this.checkDelay = ofNullable(withCheckDelay).orElse(Duration.ZERO);
+        }
+
+        private Instant nearestInstant() {
+            return Instant.now().plus(this.checkDelay);
+        }
+
+        // Проверку на expired/complete делаем не ранее, чем Instant.now(), но можем отодвинуть на checkDelay вперёд...
+        private Instant checkInstant() {
+            Instant now = Instant.now();
+            return Optional.of(nearestInstant()).filter(now::isBefore).orElse(now);
+        }
+
+        private Behavior<Command<M>> initial() {
+            return Behaviors.receive(COMMAND)
+                    .onMessage(ADD_COMMAND, this::onAddCommand)
+                    .onMessageEquals(TIMEOUT, this::processState)
+                    .build();
+        }
+
+        private Behavior<Command<M>> processState() {
+            Optional.of(timeRange.getFiredElements(nearestInstant()))
+                    .filter(Predicate.not(Collection::isEmpty))
+                    .ifPresent(firedConsumer);
+            return nextBehavior();
+        }
+
+        private Behavior<Command<M>> onAddCommand(@Nonnull AddCommand<M> command) {
+            try {
+                command.getCompletableFuture().complete(
+                        timeRange.isExpired(checkInstant()) ? command.getData() : timeRange.addElements(command.getData())
+                );
+            } catch (Throwable throwable) {
+                command.getCompletableFuture().completeExceptionally(throwable);
+            }
+            return nextBehavior();
+        }
+
+        private boolean isComplete() {
+            if (timeRange.isComplete(checkInstant())) {
+                if (timers.isTimerActive(this.timeRange)) timers.cancel(this.timeRange);
+                return true;
+            }
+            Duration duration = timeRange.duration(nearestInstant());
+            timers.startSingleTimer(this.timeRange, TIMEOUT, duration);
+            return false;
+        }
+
+        private Behavior<Command<M>> nextBehavior() {
+            return this.isComplete() ? Behaviors.stopped() : Behaviors.same();
+        }
+
+        private static class AddCommand<X> implements Command<X> {
+            private final Collection<X> data;
+            private final CompletableFuture<Collection<X>> completableFuture;
+            public AddCommand(@NonNull Collection<X> data, @NonNull CompletableFuture<Collection<X>> completableFuture) {
+                this.data = data;
+                this.completableFuture = completableFuture;
+            }
+            public @Nonnull Collection<X> getData() { return this.data; }
+            public @Nonnull CompletableFuture<Collection<X>> getCompletableFuture() { return this.completableFuture; }
+        }
+
+    }
+
+    /**
+     * Создание поведения актора при котором в случае срабатывания будильника будет вызван триггер-метод: firedConsumer,
+     * куда будут переданы сработавшие будильники
+     * @param timeRangeItems - структура, содержащая будильники с расписанием их срабатывания на заданный период
+     * @param firedConsumer - триггер-метод обработки сработавших будильников
+     * @param withCheckDuration - временной интервал на который от текущего момента изменяется момент опроса timeRangeItems
+     *                          (может быть как вперед, так и назад во времени)
+     * @param <M> - тип входящих данных, описывающих будильник
+     * @param <R> - тип исходящих данных, которые передаются в функцию обработки
+     * @return Поведение для актора-процессора временного периода с будильниками
+     */
+    static <M,R> Behavior<Command<M>> create(
+            @NonNull TimeRangeItems<M,R> timeRangeItems,
+            @NonNull Consumer<Collection<R>> firedConsumer,
+            @Nullable Duration withCheckDuration) {
+        return Behaviors.withTimers(timers -> new TimeRangeProcessor<M,R>(timers, timeRangeItems, firedConsumer, withCheckDuration).initial());
+    }
+
+    /**
+     * Создание поведения актора при котором в случае срабатывания будильника будет отправлено сообщения на actor-приёмник по обработке будильников
+     * @param timeRangeItems - структура, содержащая будильники с расписанием их срабатывания на заданный период
+     * @param firedActor - актор-приёмник сообщения со сработавшими будильниками
+     * @param responseSupplier - функция построения из списка сработавших будильников сообщение актору - подписчику
+     * @param withCheckDuration - временной интервал на который от текущего момента изменяется момент опроса timeRangeItems
+     *                          (может быть как вперед, так и назад во времени)
+     * @param <M> - тип входящих данных, описывающих будильник
+     * @param <R> - тип исходящих данных, которые передаются в функцию обработки
+     * @param <X> - тип принимаемого актором-подписчиком сообщения
+     * @return Поведение для актора-процессора временного периода с будильниками
+     */
+    static <M,R,X> Behavior<Command<M>> create(
+            @NonNull TimeRangeItems<M,R> timeRangeItems,
+            @NonNull ActorRef<X> firedActor,
+            @NonNull Function<Collection<R>,X> responseSupplier,
+            @Nullable Duration withCheckDuration) {
+        return create(
+                timeRangeItems,
+                firedElements -> ofNullable(firedElements)
+                        .filter(Predicate.not(Collection::isEmpty))
+                        .map(responseSupplier)
+                        .ifPresent(firedActor::tell),
+                withCheckDuration);
+    }
+
+    /**
+     * Отправка актору - обработчику временного периода сообщения на добавление набора элементов
+     * @param timeRangeActor актор - обработчик временного периода
+     * @param elements список добавляемых будильников
+     * @param completableFuture Completable Future ответа (принимает список непринятых элементов) 
+     * @param <M> тип отправляемого сообщения
+     */
+    static <M> void addElements(
+            @NonNull ActorRef<Command<M>> timeRangeActor, @NonNull Collection<M> elements, @NonNull CompletableFuture<Collection<M>> completableFuture
+    ) {
+        Optional.of(elements)
+                .filter(Predicate.not(Collection::isEmpty))
+                .map(data -> new TimeRangeProcessor.AddCommand<>(data, completableFuture))
+                .ifPresentOrElse(
+                        timeRangeActor::tell,
+                        () -> completableFuture.complete(Collections.emptySet())
+                );
+    }
+
+    /**
+     * Отправка актору - обработчику временного периода сообщения на добавление набора элементов
+     * @param timeRangeActor актор - обработчик временного периода
+     * @param elements список добавляемых будильников
+     * @param <M> тип отправляемого сообщения
+     * @return Completable Future ответа (принимает список непринятых элементов) 
+     */
+    @Nonnull
+    static <M> CompletableFuture<Collection<M>> addElements(
+            @NonNull ActorRef<Command<M>> timeRangeActor, @NonNull Collection<M> elements
+    ) {
+        CompletableFuture<Collection<M>> completableFuture = new CompletableFuture<>();
+        addElements(timeRangeActor, elements, completableFuture);
+        return completableFuture;
+    }
+
+    /**
+     * Отправка актору - обработчику временного периода сообщения на добавление набора элементов с игнорированием исключения
+     * @param timeRangeActor актор - обработчик временного периода
+     * @param elements список добавляемых будильников
+     * @param onCompleteWithReject метод-триггер, срабатывающая по факту добавления элементов в период обработки (в качестве параметра получит непринятые элементы)
+     * @param <M> тип отправляемого сообщения
+     */
+    static <M> void addElements(
+            @NonNull ActorRef<Command<M>> timeRangeActor, @NonNull Collection<M> elements, @NonNull Consumer<Collection<M>> onCompleteWithReject
+    ) {
+        addElements(timeRangeActor, elements).thenAccept(onCompleteWithReject);
+    }
+
+    /**
+     * Отправка актору - обработчику временного периода сообщения на добавление набора элементов с обработкой исключения
+     * @param timeRangeActor актор - обработчик временного периода
+     * @param elements список добавляемых будильников
+     * @param onCompleteWithReject метод-триггер, срабатывающая по факту добавления элементов в период обработки (в качестве параметра получит непринятые элементы)
+     * @param onThrow метод-триггер, срабатывающая по факту появления исключения при регистрации элементов в TimeRangeImages
+     * @param <M> тип отправляемого сообщения
+     */
+    static <M> void addElements(
+            @NonNull ActorRef<Command<M>> timeRangeActor, @NonNull Collection<M> elements, @NonNull Consumer<Collection<M>> onCompleteWithReject, @NonNull Consumer<Throwable> onThrow
+    ) {
+        addElements(timeRangeActor, elements).whenComplete((rejected, throwable) -> ofNullable(throwable).ifPresentOrElse(onThrow, () -> onCompleteWithReject.accept(rejected)));
+    }
+
+}
---
+++ crayfish-alarms-akka/crayfish-alarms-akka-timerange/src/test/java/com/github/sftwnd/crayfish/alarms/akka/timerange/TimeRangeTest.java
@@ -0,0 +1,163 @@
+package com.github.sftwnd.crayfish.alarms.akka.timerange;
+
+import akka.actor.testkit.typed.javadsl.ActorTestKit;
+import akka.actor.testkit.typed.javadsl.TestProbe;
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import com.github.sftwnd.crayfish.alarms.akka.timerange.TimeRange.Command;
+import com.github.sftwnd.crayfish.alarms.timerange.TimeRangeItems;
+import com.github.sftwnd.crayfish.common.expectation.ExpectedPackage;
+import com.typesafe.config.ConfigFactory;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TimeRangeTest {
+
+    static ActorTestKit testKit;
+    Instant now;
+    TimeRangeItems<ExpectedPackage<String, Instant>, String> timeRangeItems;
+
+    @Test
+    void createWithFiredConsumerAndAddElementsTest() throws ExecutionException, InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Consumer<Collection<String>> collectionConsumer = Mockito.spy(new Consumer<Collection<String>>() {
+            @Override
+            public void accept(Collection<String> ignore) {
+                countDownLatch.countDown();
+            }
+        });
+        Behavior<Command<ExpectedPackage<String, Instant>>> behavior = TimeRange.create(timeRangeItems, collectionConsumer, null);
+        ActorRef<Command<ExpectedPackage<String, Instant>>> timeRangeActor = testKit.spawn(behavior,"createWithFiredConsumerAndAddElementsTest");
+        ExpectedPackage<String, Instant> elmA = ExpectedPackage.pack("A", now);
+        ExpectedPackage<String, Instant> elmB = ExpectedPackage.pack("B", now.minus(1, ChronoUnit.MINUTES));
+        CompletableFuture<Collection<ExpectedPackage<String, Instant>>> completableFuture = TimeRange.addElements(timeRangeActor, List.of(elmA, elmB));
+        assertDoesNotThrow(completableFuture::join, "TimeRange hasn't got to throw on AddElements call");
+        Collection<String> rejected = completableFuture.get().stream().map(ExpectedPackage::getElement).collect(Collectors.toList());
+        assertEquals(List.of(elmB.getElement()), rejected, "element 'B' has to be rejected");
+        assertTrue(countDownLatch.await(750, TimeUnit.MILLISECONDS), "collectionConsumer has to be called");
+        @SuppressWarnings("unchecked")
+        ArgumentCaptor<Collection<String>> argumentCaptor = ArgumentCaptor.forClass(Collection.class);
+        Mockito.verify(collectionConsumer, Mockito.times(1)).accept(argumentCaptor.capture());
+        assertEquals(Set.of(elmA.getElement()), argumentCaptor.getValue(), "element 'A' has to be processed by TimeRange actor");
+    }
+
+    @Test
+    void createWithFiredActorAndAddElementsTest() throws InterruptedException {
+        TestProbe<String> testProbe = testKit.createTestProbe(String.class);
+        try {
+            ActorRef<String> actorRef = testProbe.getRef();
+            Behavior<Command<ExpectedPackage<String, Instant>>> behavior = TimeRange.create(timeRangeItems, actorRef, str -> "OK:" + str, null);
+            ActorRef<Command<ExpectedPackage<String, Instant>>> timeRangeActor = testKit.spawn(behavior, "createWithFiredActorAndAddElementsTest");
+            ExpectedPackage<String, Instant> elmC = ExpectedPackage.pack("C", now);
+            ExpectedPackage<String, Instant> elmD = ExpectedPackage.pack("D", now.minus(1, ChronoUnit.MINUTES));
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+            List<String> rejected = new ArrayList<>();
+            TimeRange.addElements(timeRangeActor, List.of(elmC, elmD), rejects -> {
+                rejects.forEach(pack -> rejected.add(pack.getElement()));
+                countDownLatch.countDown();
+            });
+            //noinspection ResultOfMethodCallIgnored
+            countDownLatch.await(750, TimeUnit.MILLISECONDS);
+            assertEquals(List.of(elmD.getElement()), rejected, "element 'D' has to be rejected");
+
+        } finally {
+            testProbe.stop();
+        }
+    }
+
+    @SneakyThrows
+    <R> R throwOnResult() {
+        throw new Throwable();
+    }
+
+    @Test
+    void createThrowsOnAddElementsTest() {
+        TestProbe<String> testProbe = testKit.createTestProbe(String.class);
+        try {
+            ActorRef<String> actorRef = testProbe.getRef();
+            @SuppressWarnings("unchecked")
+            TimeRangeItems<ExpectedPackage<String, Instant>, String> timeRangeItems = Mockito.mock(TimeRangeItems.class);
+            Mockito.when(timeRangeItems.addElements(Mockito.any())).thenAnswer(elements -> throwOnResult());
+            Behavior<Command<ExpectedPackage<String, Instant>>> behavior = TimeRange.create(timeRangeItems, actorRef, str -> "OK:" + str, null);
+            ActorRef<Command<ExpectedPackage<String, Instant>>> timeRangeActor = testKit.spawn(behavior, "createThrowsOnAddElementsTest");
+            ExpectedPackage<String, Instant> elmX = ExpectedPackage.pack("X", now);
+            CompletableFuture<Collection<ExpectedPackage<String, Instant>>> completableFuture = new CompletableFuture<>();
+            TimeRange.addElements(timeRangeActor, List.of(elmX), completableFuture::complete, completableFuture::completeExceptionally);
+            assertThrows(Throwable.class, completableFuture::join, "After throw on TimeRange::addElements onThrow consumer has to be fired");
+        } finally {
+            testProbe.stop();
+        }
+    }
+
+    @Test
+    void addEmptyElements() {
+        Behavior<Command<ExpectedPackage<String, Instant>>> behavior = TimeRange.create(timeRangeItems, elements -> {}, null);
+        ActorRef<Command<ExpectedPackage<String, Instant>>> timeRangeActor = testKit.spawn(behavior,"addEmptyElements");
+        CompletableFuture<Collection<ExpectedPackage<String, Instant>>> completableFuture = TimeRange.addElements(timeRangeActor, Collections.emptyList());
+        assertDoesNotThrow(completableFuture::join, "TimeRange hasn't got to throw on AddElements with empty list call");
+        assertEquals(Collections.emptySet(), completableFuture.join(), "AddElements has return empty list for the empty request");
+    }
+
+    @Test
+    void testAddOnExpired() {
+        now = Instant.now().minus(1, ChronoUnit.MINUTES);
+        timeRangeItems = TimeRangeItems.constructPackable(now, Duration.ofMinutes(-1), Duration.ofSeconds(5), Duration.ofMillis(255), Duration.ofSeconds(15), null);
+        Behavior<Command<ExpectedPackage<String, Instant>>> behavior = TimeRange.create(timeRangeItems, elements -> {}, null);
+        ActorRef<Command<ExpectedPackage<String, Instant>>> timeRangeActor = testKit.spawn(behavior,"testAddOnCompleted");
+        ExpectedPackage<String, Instant> elmX = ExpectedPackage.pack("X", now.minus(30, ChronoUnit.SECONDS));
+        assertTrue(elmX.happened(timeRangeItems.getLastInstant().minusMillis(1)), "elmX hasn't got to be happend near the end of timeRange");
+        assertFalse(elmX.happened(timeRangeItems.getStartInstant()), "elmX has to be happend on the end of timeRange");
+        assertTrue(timeRangeItems.isExpired(), "TimeRange period has to be expired");
+        CompletableFuture<Collection<ExpectedPackage<String, Instant>>> completableFuture = TimeRange.addElements(timeRangeActor, List.of(elmX));
+    }
+
+    @BeforeEach
+    void timeRangeItems() {
+        this.now = Instant.now().truncatedTo(ChronoUnit.MINUTES);
+        timeRangeItems = TimeRangeItems.constructPackable(now, Duration.ofSeconds(60), Duration.ofSeconds(5), Duration.ofMillis(255), Duration.ofSeconds(15), null);
+    }
+
+    @BeforeEach
+    void clean() {
+        this.now = null;
+        timeRangeItems = null;
+    }
+
+    @BeforeAll
+    static void startUp() {
+        testKit = ActorTestKit.create(ConfigFactory.load("akka-test.conf"));
+    }
+
+    @AfterAll
+    static void tearDown() {
+        testKit.shutdownTestKit();
+        testKit = null;
+    }
+
+}
