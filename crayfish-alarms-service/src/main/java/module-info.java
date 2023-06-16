6bfe6caa931167f8731231c3a1e673534d57d9e7
Andrey Shindarev <ashindarev@gmail.com>
2023-06-16 17:20:20 +0300

    Change lombok dependency to static

--- crayfish-alarms-service/src/main/java/module-info.java
+++ crayfish-alarms-service/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 open module com.github.sftwnd.crayfish_alarms_service {
     requires transitive com.github.sftwnd.crayfish_alarms_timerange;
+    requires static lombok;
     requires java.logging;
-    requires lombok;
     exports com.github.sftwnd.crayfish.alarms.service;
 }
--- crayfish-alarms-timerange/src/test/java/module-info.java
+++ crayfish-alarms-timerange/src/test/java/module-info.java
@@ -1,6 +1,6 @@
 open module com.github.sftwnd.crayfish_alarms_timerange.test {
     requires com.github.sftwnd.crayfish_alarms_timerange;
-    requires lombok;
+    requires static lombok;
     requires org.junit.jupiter.api;
     requires org.mockito;
 }
